ae85584cfa9a9e5ee96a1aeb65cb426f5db5c74b
IrenPischulina <iren.pischulina@gmail.com>
2019-04-21 16:31:10 +0700

    Реализовано перемещение элемента матрицы вниз

--- game.py
+++ game.py
@@ -9,3 +9,11 @@ class Game(object):
                 num += 1
             self.field.append(line)
         self.field[3][3] = 0
+
+    def move_down(self):
+        for i in range(4):
+            for j in range(4):
+                if self.field[i][j] == 0:
+                    if i != 0:
+                        self.field[i][j] = self.field[i-1][j]
+                        self.field[i-1][j] = 0
