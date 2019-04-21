2a80fe35c4f693da82c7ae855debea16a87ff7e9
IrenPischulina <iren.pischulina@gmail.com>
2019-04-21 18:29:18 +0700

    Реализовано перемещение элемента матрицы вверх, вправо и влево

--- game.py
+++ game.py
@@ -17,3 +17,30 @@ class Game(object):
                     if i != 0:
                         self.field[i][j] = self.field[i-1][j]
                         self.field[i-1][j] = 0
+
+    def move_left(self):
+        for i in range(4):
+            for j in range(4):
+                if self.field[i][j] == 0:
+                    if j != 3:
+                        self.field[i][j] = self.field[i][j + 1]
+                        self.field[i][j + 1] = 0
+                        return
+
+    def move_right(self):
+        for i in range(4):
+            for j in range(4):
+                if self.field[i][j] == 0:
+                    if j != 0:
+                        self.field[i][j] = self.field[i][j - 1]
+                        self.field[i][j - 1] = 0
+                        return
+
+    def move_up(self):
+        for i in range(4):
+            for j in range(4):
+                if self.field[i][j] == 0:
+                    if i != 3:
+                        self.field[i][j] = self.field[i + 1][j]
+                        self.field[i + 1][j] = 0
+                        return
