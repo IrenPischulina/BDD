cf919a59a05385aea8b8c0dd139a0d8a7ca4955e
IrenPischulina <iren.pischulina@gmail.com>
2019-04-22 16:00:55 +0700

    Реализован выигрыш

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-import random
+import random, getch, os
 
 
 class Game(object):
@@ -59,3 +59,49 @@ class Game(object):
                 self.move_right()
             elif move == 3:
                 self.move_up()
+
+    def show(self):
+        for i in range(4):
+            for j in range(4):
+                if game.field[i][j] < 10:
+                    print(" ", end='')
+                    print(game.field[i][j], end='')
+                    print(' ', end='')
+                else:
+                    print(game.field[i][j], end='')
+                    print(' ', end='')
+            print("\n", end='')
+        return
+
+    def isWin(self):
+        for i in range(4):
+            for j in range(4):
+                if i != 3 and j != 3:
+                    if self.field[i][j] != j + i * 4 + 1:
+                        return False
+        if self.field[3][3] != 0:
+            return False
+        return True
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.mix()
+    key = 0
+    while key != 'z':
+        print()
+        os.system('clear')
+        game.show()
+        if game.isWin():
+            print("Победа!!!")
+            break
+        key = getch.getch()
+        if key == 'a':
+            game.move_left()
+        elif key == 's':
+            game.move_down()
+        elif key == 'd':
+            game.move_right()
+        elif key == 'w':
+            game.move_up()
+
